0f2820809c892293f1223bb7d2d3f60853749b52
Karl-0-1 <keshabagarwal01902@gmail.com>
2025-10-02 18:53:01 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -10,11 +10,11 @@ housing = fetch_california_housing()
 X = pd.DataFrame(housing.data, columns=housing.feature_names)
 y = pd.Series(housing.target)
 
-# For this quick project, we'll use a few key features to keep the UI simple
+# May select more features. I used less bcoz the model becomes more and more heavy
 features = ['MedInc', 'HouseAge', 'AveRooms', 'Population']
 X = X[features]
 
-# 2. Split data (optional for this sprint, but good practice)
+# 2. Splitting dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 3. Train the model
